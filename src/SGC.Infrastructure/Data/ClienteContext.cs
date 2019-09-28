ee3df0c76c82fcba3573de5b643c7204ae0a7c0f
Qoppa <luiiz.bgs@gmail.com>
2019-09-27 23:34:16 -0300

    Criando classe Classe cliente context e migration

---
+++ src/SGC.Infrastructure/Data/ClienteContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.AppCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Infrastructure.Data
+{
+   public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+
+        }
+    }
+}
--- src/SGC.UI/Startup.cs
+++ src/SGC.UI/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SGC.Infrastructure.Data;
 
 namespace SGC.UI
 {
@@ -30,7 +32,8 @@ namespace SGC.UI
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            //Atenção aqui luiz e na "DefaultConnection" criada na appsenttings.json
+            services.AddDbContext<ClienteContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
