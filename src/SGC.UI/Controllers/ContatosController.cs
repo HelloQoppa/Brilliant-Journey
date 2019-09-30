0949cbddadc957a6a26376928f1a7d5f072b1f23
Qoppa <luiiz.bgs@gmail.com>
2019-09-30 12:20:01 -0300

    Criação do Controller e views das entidades

---
+++ src/SGC.UI/Controllers/ContatosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SGC.AppCore.Entity;
+using SGC.Infrastructure.Data;
+
+namespace SGC.UI.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private readonly ClienteContext _context;
+
+        public ContatosController(ClienteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contatos
+        public async Task<IActionResult> Index()
+        {
+            var clienteContext = _context.Contatos.Include(c => c.Cliente);
+            return View(await clienteContext.ToListAsync());
+        }
+
+        // GET: Contatos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contatos
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.ContatoId == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contato);
+        }
+
+        // GET: Contatos/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteId", "CPF");
+            return View();
+        }
+
+        // POST: Contatos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContatoId,Nome,Telefone,Email,ClienteId")] Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contato);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteId", "CPF", contato.ClienteId);
+            return View(contato);
+        }
+
+        // GET: Contatos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contatos.FindAsync(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteId", "CPF", contato.ClienteId);
+            return View(contato);
+        }
+
+        // POST: Contatos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContatoId,Nome,Telefone,Email,ClienteId")] Contato contato)
+        {
+            if (id != contato.ContatoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contato);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContatoExists(contato.ContatoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteId", "CPF", contato.ClienteId);
+            return View(contato);
+        }
+
+        // GET: Contatos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contato = await _context.Contatos
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.ContatoId == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return View(contato);
+        }
+
+        // POST: Contatos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contato = await _context.Contatos.FindAsync(id);
+            _context.Contatos.Remove(contato);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContatoExists(int id)
+        {
+            return _context.Contatos.Any(e => e.ContatoId == id);
+        }
+    }
+}
---
+++ src/SGC.UI/Controllers/ProfissasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SGC.AppCore.Entity;
+using SGC.Infrastructure.Data;
+
+namespace SGC.UI.Controllers
+{
+    public class ProfissasController : Controller
+    {
+        private readonly ClienteContext _context;
+
+        public ProfissasController(ClienteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Profissas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Profissoes.ToListAsync());
+        }
+
+        // GET: Profissas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profissao = await _context.Profissoes
+                .FirstOrDefaultAsync(m => m.ProfissaoId == id);
+            if (profissao == null)
+            {
+                return NotFound();
+            }
+
+            return View(profissao);
+        }
+
+        // GET: Profissas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profissas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProfissaoId,Nome,Descricao,CBO")] Profissao profissao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(profissao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profissao);
+        }
+
+        // GET: Profissas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profissao = await _context.Profissoes.FindAsync(id);
+            if (profissao == null)
+            {
+                return NotFound();
+            }
+            return View(profissao);
+        }
+
+        // POST: Profissas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProfissaoId,Nome,Descricao,CBO")] Profissao profissao)
+        {
+            if (id != profissao.ProfissaoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(profissao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfissaoExists(profissao.ProfissaoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(profissao);
+        }
+
+        // GET: Profissas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profissao = await _context.Profissoes
+                .FirstOrDefaultAsync(m => m.ProfissaoId == id);
+            if (profissao == null)
+            {
+                return NotFound();
+            }
+
+            return View(profissao);
+        }
+
+        // POST: Profissas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var profissao = await _context.Profissoes.FindAsync(id);
+            _context.Profissoes.Remove(profissao);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfissaoExists(int id)
+        {
+            return _context.Profissoes.Any(e => e.ProfissaoId == id);
+        }
+    }
+}
